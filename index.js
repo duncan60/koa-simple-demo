b595a25c911b9bdd68fb5b76694f1c27786be1ff
Duncan_Du <tmduncan60@gmail.com>
2017-04-28 11:04:24 +0800

    add router

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const Koa  = require('koa');
+const router = require('koa-router')();
+const bodyParser = require('koa-bodyparser');
+
 const app = new Koa();
 
-app.use(ctx => {
-  ctx.body = 'Hello Koa';
+router.get('/hello/:name', async (ctx, next) => {
+    const name = ctx.params.name;
+    ctx.response.body = `<h1>Hello, ${name}!</h1>`;
 });
+
+router.get('/user', async (ctx, next) => {
+    ctx.response.type = 'application/json';
+    ctx.response.body = {
+            user: {
+                name: 'duncan',
+                age: '28',
+                gender: 'man'
+            }
+    };
+});
+
+app.use(bodyParser());
+app.use(router.routes());
 
 app.listen(3000);
